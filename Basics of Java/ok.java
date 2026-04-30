7409909f8c935a864435b701415e001af6f4ab27
sanjogwiz01 <sanjogpoudel384@gmail.com>
2026-04-30 21:22:15 +0545

    started the java'

--- Basics of Java/ok.java
+++ Basics of Java/ok.java
@@ -3,3 +3,4 @@ class ok {
         System.out.println("Hello World");
     }
 }
+// this is is basic code to pirnt hello world in the jav a programming language. The class is named "ok" and it contains the main method which is the entry point of the program. When executed, it will print "Hello World" to the console.
