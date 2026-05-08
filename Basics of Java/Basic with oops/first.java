76baab10c507b0fdd97856541c36ccfb99f547d7
sanjogwiz01 <sanjogpoudel384@gmail.com>
2026-05-08 10:09:20 +0545

    modified place

--- Basics of Java/Basic with oops/first.java
+++
@@ -1,6 +0,0 @@
-public class first{
-    public static void main(String[] args){
-        System.out.println("Hello World");
-        System.out.println("hello i am learning java");
-    }
-}
--- Basics of Java/ok.java
+++
@@ -1,7 +0,0 @@
-class ok {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
-// this is is basic code to pirnt hello world in the jav a programming language. The class is named "ok" and it contains the main method which is the entry point of the program.
-// When executed, it will print "Hello World" to the console.
