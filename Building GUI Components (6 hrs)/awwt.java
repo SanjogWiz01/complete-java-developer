90f14846e719772bbab4cf274f9ba7be77f8ab3d
sanjogwiz01 <sanjogpoudel384@gmail.com>
2026-05-28 19:28:18 +0545

    commited

---
+++ Building GUI Components (6 hrs)/awwt.java
@@ -0,0 +1,13 @@
+import java.awt.Frame;
+
+public class awwt {
+    public static void main(String[] args) {
+
+        Frame f = new Frame("My First AWT App");
+
+        f.setSize(400, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setVisible(true);
+    }
+}
