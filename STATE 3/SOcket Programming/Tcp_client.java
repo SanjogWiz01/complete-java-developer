331a157cf843062f1987b17ad17f1266d8286c54
sanjogwiz01 <sanjogpoudel384@gmail.com>
2026-06-21 21:55:38 +0545

    tcp client

---
+++ STATE 3/SOcket Programming/Tcp_client.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.net.*;
+
+public class Tcp_client {
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("localhost", 5000);
+        System.out.println("Connected to server");
+        
+        // Send message to server
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        out.println("Hello from client!");
+        
+        // Read response from server
+        BufferedReader in = new BufferedReader(
+            new InputStreamReader(socket.getInputStream())
+        );
+        String response = in.readLine();
+        System.out.println("Server says: " + response);
+        
+        socket.close();
+    }
+}
