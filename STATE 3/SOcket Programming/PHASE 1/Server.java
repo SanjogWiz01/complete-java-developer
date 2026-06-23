9568eb2ccce058b51960469cf1110f8518ac5fae
sanjogwiz01 <sanjogpoudel384@gmail.com>
2026-06-23 22:02:04 +0545

    server added

---
+++ STATE 3/SOcket Programming/PHASE 1/Server.java
@@ -0,0 +1,31 @@
+import java.net.*;
+import java.io.*;
+
+public class Server {
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(5000);
+            System.out.println("Server is waiting...");
+
+            Socket socket = serverSocket.accept();
+            System.out.println("Client connected.");
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()));
+
+            PrintWriter out = new PrintWriter(
+                    socket.getOutputStream(), true);
+
+            String message = in.readLine();
+            System.out.println("Client: " + message);
+
+            out.println("Hello from Server");
+
+            socket.close();
+            serverSocket.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
