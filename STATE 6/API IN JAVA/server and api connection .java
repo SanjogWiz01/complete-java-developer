f7fb365284114b5f15e9bc87b63f8f1503914265
sanjogwiz01 <sanjogpoudel384@gmail.com>
2026-06-27 22:20:58 +0545

    java web servlets

---
+++ STATE 6/API IN JAVA/server and api connection .java
@@ -0,0 +1,19 @@
+URL url = new URL("https://example.com/api");
+HttpURLConnection con = (HttpURLConnection) url.openConnection();
+con.setRequestMethod("POST");
+con.setRequestProperty("Content-Type", "application/json; utf-8");
+con.setDoOutput(true);
+String jsonInputString = "{\"name\": \"John\", \"age\": 30}";
+try(OutputStream os = con.getOutputStream()) {
+    byte[] input = jsonInputString.getBytes("utf-8");
+    os.write(input, 0, input.length);
+}
+try(BufferedReader br = new BufferedReader(
+  new InputStreamReader(con.getInputStream(), "utf-8"))) {
+    StringBuilder response = new StringBuilder();
+    String responseLine;
+    while ((responseLine = br.readLine()) != null) {
+        response.append(responseLine.trim());
+    }
+    System.out.println(response.toString());
+}
