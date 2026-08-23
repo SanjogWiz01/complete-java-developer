1e65dc0c9a9b6f5351f1ef903c1b75290840b01e
sanjogwiz01 <sanjogpoudel384@gmail.com>
2026-08-23 20:00:54 +0545

    Set random BCrypt password for seeded demo users

--- BANK/src/main/resources/db/seed.sql
+++ BANK/src/main/resources/db/seed.sql
@@ -7,9 +7,9 @@ INSERT INTO customers (
 ('Ravi', 'Thapa', '1990-08-25', 'NID-10002', 'ravi.thapa@example.com', '+977-9800000002', 'Lakeside', 'Pokhara', 'Gandaki', '33700');
 
 INSERT INTO users (username, password_hash, role, customer_id, is_active) VALUES
-('admin', '$2a$12$W1af7xUtdv4P1Kz3zP47A.FKk2Q6HOrxxF5ovP4pA8ROgGQrm2gwG', 'ADMIN', NULL, TRUE),
-('jyoti', '$2a$12$W1af7xUtdv4P1Kz3zP47A.FKk2Q6HOrxxF5ovP4pA8ROgGQrm2gwG', 'CUSTOMER', 1, TRUE),
-('ravi', '$2a$12$W1af7xUtdv4P1Kz3zP47A.FKk2Q6HOrxxF5ovP4pA8ROgGQrm2gwG', 'CUSTOMER', 2, TRUE);
+('admin', '$2a$12$yN546rEl2b5ZzXajsJSE9.2bzg/zWRnP4LggyWkJa48RgMkd2X5re', 'ADMIN', NULL, TRUE),
+('jyoti', '$2a$12$yN546rEl2b5ZzXajsJSE9.2bzg/zWRnP4LggyWkJa48RgMkd2X5re', 'CUSTOMER', 1, TRUE),
+('ravi', '$2a$12$yN546rEl2b5ZzXajsJSE9.2bzg/zWRnP4LggyWkJa48RgMkd2X5re', 'CUSTOMER', 2, TRUE);
 
 INSERT INTO accounts (account_number, customer_id, account_type, balance, status, interest_rate, minimum_balance) VALUES
 ('SAV20260821123401', 1, 'SAVINGS', 25000.00, 'ACTIVE', 0.0450, 500.00),
